940af92bc4d5fb4c558651364e3db629e40c6846
Rena Feng <68396709+renapfeng@users.noreply.github.com>
2021-06-22 14:56:48 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} The Global Dialogue Initiative /*&bull; Crafted with <span role="img" aria-label="love">❤️</span>*/</p>
+        <p>&copy; {new Date().getFullYear()} The Global Dialogue Initiative/*&bull; Crafted with <span role="img" aria-label="love">❤️</span>*/</p>
       </footer>
     </div>
   )
